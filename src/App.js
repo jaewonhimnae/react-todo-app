2aad6b517932c2b7f3541eacb094bff08f07f817
johnahn7 <johnahn@fitfuns.com>
2021-11-03 19:46:20 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let todoDatas = [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true,
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false,
+    },
+  ]
+
+  const handleClick = (id) => {
+   let newTodoDatas = todoDatas.filter(data => data.id !== id);
+    console.log('newTodoDatas',newTodoDatas);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='todoBlock'>
+        <div className="title">
+          <h1>할 일 목록</h1>
+        </div>
+
+        {todoDatas.map(data => (
+          <div style={listStyle} key={data.id}>
+            <p>
+              <input type="checkbox" defaultChecked={data.completed} />
+              {" "}{data.title}
+              <button style={btnStyle} onClick={() =>handleClick(data.id)}>x</button>
+            </p>
+          </div>
+        ))}
+
+      </div>
     </div>
   );
 }
 
+const listStyle = {
+  padding: "10px",
+  borderBottom: "1px #ccc dotted",
+}
+
+const btnStyle = {
+  color: "#fff",
+  border: "none",
+  padding: "5px 9px",
+  borderRadius: "50%",
+  cursor: "pointer",
+  float: "right",
+};
+
 export default App;
+
+
